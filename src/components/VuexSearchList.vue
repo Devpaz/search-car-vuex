62ad8f746429ffd23e6b7ad3ff09b5e6f3faa251
Paolo Paz <sl.paolo.paz@gmail.com>
2022-08-21 15:43:15 -0500

    Develop the logic of the VuexSearchList component

--- src/components/VuexSearchList.vue
+++ src/components/VuexSearchList.vue
@@ -1,9 +1,35 @@
 <template>
     <div>
-        test
+        
     </div>
 </template>
 
-<style lang = "stylus">
+<script>
+    import {mapGetters} from 'vuex'
+    export default {
+      data () {
+        return {
+          numberOfColumns: 3
+        }
+      },
+      computed: {
+        ...mapGetters(['filteredCars'])
+      },
+      methods: {
+        available (bool) {
+          return bool ? 'Sí' : 'No'
+        } ,
+        carModel (carModel) {
+          return `Modelo: ${carModel}`
+        },
+        blockRandom () {
+          return `block-${Math.floor(Math.random() * 6) + 1}`
+        }
+      }
+    }
+</script>
+
+
+<style lang="stylus">
     @import "../assets/search.styl"
 </style>
