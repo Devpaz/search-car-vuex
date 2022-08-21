fb7f3cf1525e44f4af56a8e93065fa72907c912e
Paolo Paz <sl.paolo.paz@gmail.com>
2022-08-21 15:56:13 -0500

    Develop the VuexSearchList template, with various types of fields

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <vuex-search-list></vuex-search-list>
     <vuex-search-form></vuex-search-form>
+    <vuex-search-list></vuex-search-list>
   </div>
 </template>
 
