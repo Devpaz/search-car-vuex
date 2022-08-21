b95438ea5571fa7ff19dcc6a610d7c178386ebcd
Paolo Paz <sl.paolo.paz@gmail.com>
2022-08-21 15:25:25 -0500

    Develop the VuexSearchForm template, with various types of fields

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <vuex-search-list></vuex-search-list>
+    <vuex-search-form></vuex-search-form>
   </div>
 </template>
 
 <script>
 import VuexSearchList from './components/VuexSearchList.vue'
+import VuexSearchForm from './components/VuexSearchForm.vue'
 
 export default {
   name: 'App',
   components: {
-    VuexSearchList
+    VuexSearchList,
+    VuexSearchForm
   }
 }
 </script>
